d2d20d5029d3691f8502586a4359b145b04bd3d6
Jay <me@jayxtq.xyz>
2024-08-22 23:01:47 +0100

    feat: friday plugin release

---
+++ Friday/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace Friday {
+    using Exiled.API.Interfaces;
+    
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public string Token { get; set; } = "your_token_here";
+    }
+}
---
+++ Friday/Friday.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Handlers = Exiled.Events.Handlers;
+
+namespace Friday
+{
+    public class Friday: Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            Handlers.Server.LocalReporting += LocalReporting;
+        }
+    
+        public override void OnDisabled()
+        {
+            Handlers.Server.LocalReporting -= LocalReporting;
+        }
+
+        void LocalReporting(LocalReportingEventArgs ev)
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            client.Headers.Add("Authorization", Config.Token);
+            var jsonData = new
+            {
+                reporterName = ev.Player.Nickname,
+                reporterId = ev.Player.UserId,
+                reportedName = ev.Target.Nickname,
+                reportedId = ev.Target.UserId,
+                reason = ev.Reason
+            };
+            client.UploadData("https://friday.jayxtq.xyz/report", "POST", Encoding.UTF8.GetBytes(jsonData.ToString()));
+        }
+    }
+}
