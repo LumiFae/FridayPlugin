bd9a3e1ec9e668825f56c3667351a3bd391d42a9
Jay <me@jayxtq.xyz>
2024-10-11 22:45:35 +0100

    fix: desync

--- Friday/EventHandler.cs
+++ Friday/EventHandler.cs
@@ -1,11 +1,8 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Exiled.API.Features;
-using Newtonsoft.Json;
+﻿using System.Security.Cryptography.X509Certificates;
 using PluginAPI.Core.Attributes;
 using PluginAPI.Enums;
 using PluginAPI.Events;
+using MEC;
 
 namespace Friday
 {
@@ -14,34 +11,7 @@ namespace Friday
         [PluginEvent(ServerEventType.PlayerReport)]
         public void PlayerReport(PlayerReportEvent ev)
         {
-            Log.Info("Report incoming...");
-            var jsonData = new
-            {
-                reporterName = ev.Player.Nickname,
-                reporterId = ev.Player.UserId,
-                reportedName = ev.Target.Nickname,
-                reportedId = ev.Target.UserId,
-                reason = ev.Reason,
-                serverName = Server.Name,
-                serverType = 0
-            };
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Plugin.Instance.Config.Token);
-                var content = new StringContent(JsonConvert.SerializeObject(jsonData));
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                var response = client.PostAsync("https://friday.jayxtq.xyz/report",
-                    content).GetAwaiter().GetResult();
-                var responseString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                if (response.StatusCode != HttpStatusCode.OK)
-                {
-                    Log.Error("Error from server: " + responseString);
-                }
-                else
-                {
-                    Log.Info(responseString);
-                }
-            }
+            Timing.RunCoroutine(Plugin.Instance.ReportPlayer(ev));
         }
     }
 }
--- Friday/Plugin.cs
+++ Friday/Plugin.cs
@@ -1,4 +1,9 @@
-﻿using Exiled.API.Features;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Exiled.API.Features;
+using Newtonsoft.Json;
 using PluginAPI.Events;
 
 namespace Friday
@@ -22,6 +27,40 @@ namespace Friday
             EventManager.UnregisterEvents(this, handler);
             handler = null;
             base.OnDisabled();
+        }
+        
+        public IEnumerator<float> ReportPlayer(PlayerReportEvent ev)
+        {
+            Log.Info("Report incoming...");
+            var jsonData = new
+            {
+                reporterName = ev.Player.Nickname,
+                reporterId = ev.Player.UserId,
+                reportedName = ev.Target.Nickname,
+                reportedId = ev.Target.UserId,
+                reason = ev.Reason,
+                serverName = Server.Name,
+                serverType = 0
+            };
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Plugin.Instance.Config.Token);
+                var content = new StringContent(JsonConvert.SerializeObject(jsonData));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = client.PostAsync("https://friday.jayxtq.xyz/report",
+                    content).GetAwaiter().GetResult();
+                var responseString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    Log.Error("Error from server: " + responseString);
+                }
+                else
+                {
+                    Log.Info(responseString);
+                }
+            }
+
+            yield return 0;
         }
     }
 }
